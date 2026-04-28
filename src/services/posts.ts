197516f6739d9f092728110f87a104e4a4a658b8
Luci <lapl1412.work@gmail.com>
2026-04-28 21:33:26 +0700

    refactor: update BASE_URL to use environment variable instead of hardcoded value

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,5 +1,6 @@
 // const BASE_URL = typeof window !== 'undefined' ? '/api' : (import.meta.env.PUBLIC_API_URL);
-const BASE_URL = "https://luci-web-blog-backend.fly.dev";
+const BASE_URL = import.meta.env.PUBLIC_API_URL;
+// const BASE_URL = "https://luci-web-blog-backend.fly.dev";
 
 export interface Post {
     id: number;
